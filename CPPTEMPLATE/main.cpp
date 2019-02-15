0f3316cb7f9ca2fc9215cfe20458815cb9ac1b13
Dredz223 <42884620+Dredz223@users.noreply.github.com>
2019-02-15 15:46:31 -0800

    Template
    Added the C++ Template

---
+++ CPPTEMPLATE/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Andres Guerrero Maldonado
+ * Created on September 18, 2018 7:58PM
+ * Purpose:  CPP Template
+ *         
+ */
+
+//System Libraries
+#include <iostream> //I/O Library -> cout,endl
+#include <string>   //String Library
+#include <iomanip>  //Format Library
+using namespace std;//namespace I/O stream library created
+
+//User Libraries
+
+//Global Constants
+//Math, Physics, Science, Conversions, 2-D Array Columns
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    //Initial Variables
+    
+    //Map/Process Inputs to Outputs
+
+    //Exit program!
+    return 0;
+}
